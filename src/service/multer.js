1245b5dec63aefb80a6ea35ffa3c31e0f73343f3
maged <maged.atef.arteen@gmail.com>
2025-03-24 11:55:21 +0200

    temp folder fix

--- src/service/multer.js
+++ src/service/multer.js
@@ -11,23 +11,23 @@ import { fileURLToPath } from 'url';
 
 export const multerLocal = () => {
 
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename);
+    // const __filename = fileURLToPath(import.meta.url);
+    // const __dirname = path.dirname(__filename);
 
-    const uploadDir = path.join(__dirname, '../uploads'); // Adjust path if needed
-    if (!fs.existsSync(uploadDir)) {
-        fs.mkdirSync(uploadDir, { recursive: true }); // Ensure folder exists
-    }
+    // const uploadDir = path.join(__dirname, '../uploads'); // Adjust path if needed
+    // if (!fs.existsSync(uploadDir)) {
+    //     fs.mkdirSync(uploadDir, { recursive: true }); // Ensure folder exists
+    // }
 
-    const storage = multer.diskStorage({
-        destination: function (req, file, cb) {
-            cb(null, uploadDir);
-        },
-        filename: function (req, file, cb) {
-            cb(null, nanoid(5) + '-' + file.originalname);
-        }
-    });
-
+    // const storage = multer.diskStorage({
+    //     destination: function (req, file, cb) {
+    //         cb(null, uploadDir);
+    //     },
+    //     filename: function (req, file, cb) {
+    //         cb(null, nanoid(5) + '-' + file.originalname);
+    //     }
+    // });
+    const storage = multer.diskStorage({})
     const fileFilter = (req, file, cb) => {
         if (file.mimetype === 'application/pdf' || file.mimetype.startsWith('image/' || file.mimetype == 'application/text')) {
             cb(null, true);
